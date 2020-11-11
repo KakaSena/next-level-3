a80b0d9fd0039cfa6f49168c5a16a79d6bd3e053
Ricardo Sena <ricardoxsena@gmail.com>
2020-11-11 15:45:31 -0300

    database setup and working

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
 
 app.use(express.json());
 
 app.get('/users', (request, response) => {
-    return response.json({ message: 'hello world' });
+    return response.json({message: "hello world"});
 });
 
-
-app.listen(3333); 
+app.listen(3333);
